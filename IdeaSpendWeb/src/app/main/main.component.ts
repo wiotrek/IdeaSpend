c68688a6c98b62b2cef9a22af1cac9e52e369315
Kipoderax <sebastian.rajczyk@wp.pl>
2021-02-14 16:16:38 +0100

    closes #13

--- IdeaSpendWeb/src/app/main/main.component.ts
+++ IdeaSpendWeb/src/app/main/main.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  // TODO: change font family for information inside square
+
+  greenTriangleImage = '../assets/main/green-triangle.png';
+  redTriangleImage = '../assets/main/red-triangle.png';
+
   constructor() { }
 
   ngOnInit(): void {
