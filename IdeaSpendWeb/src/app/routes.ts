730dd53f80a7ee0c3f9c364c15c63fb16c08b4a6
Wiotrek <piotrwaszak97@gmail.com>
2021-02-15 18:23:31 +0100

    duplicate path - login in routes.ts

--- IdeaSpendWeb/src/app/routes.ts
+++ IdeaSpendWeb/src/app/routes.ts
@@ -10,7 +10,6 @@ import { TransactionsComponent } from './transactions/transactions.component';
 export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
   { path: '', component: MainComponent },
   { path: 'products', component: ProductComponent },
   { path: 'statistics', component: StatisticsComponent },
