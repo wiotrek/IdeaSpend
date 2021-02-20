54954f7010b2f69f761d5aee59c5831ba588da81
Kipoderax <sebastian.rajczyk@wp.pl>
2021-02-20 16:38:44 +0100

    close #50

--- IdeaSpendWeb/src/app/app.module.ts
+++ IdeaSpendWeb/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { AuthService } from './_services/auth.service';
+import { AuthGuard } from './_guards/AuthGuard';
+import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
 import { NavigationComponent } from './navigation/navigation.component';
+
 import { MainComponent } from './main/main.component';
-import { ProductComponent } from './product/product.component';
+
 import { StatisticsComponent } from './statistics/statistics.component';
+
 import { TransactionsOverviewComponent } from './transactions/transactions-overview/transactions-overview.component';
 import { TransactionsNewComponent } from './transactions/transactions-new/transactions-new.component';
-import { AuthGuard } from './_guards/AuthGuard';
+
+import { ProductComponent } from './product/product.component';
 import { ProductOverviewComponent } from './product/product-overview/product-overview.component';
 import { ProductCatalogsComponent } from './product/product-catalogs/product-catalogs.component';
 import { ProductAddComponent } from './product/product-add/product-add.component';
@@ -28,10 +34,10 @@ import { ProductAddComponent } from './product/product-add/product-add.component
     RegisterComponent,
     NavigationComponent,
     MainComponent,
-    ProductComponent,
     StatisticsComponent,
     TransactionsOverviewComponent,
     TransactionsNewComponent,
+    ProductComponent,
     ProductOverviewComponent,
     ProductCatalogsComponent,
     ProductAddComponent,
