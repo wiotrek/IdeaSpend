3868767fcd38f68e41fde360cb4599d49d0446ab
Kipoderax <sebastian.rajczyk@wp.pl>
2021-03-03 10:29:17 +0100

    close #75

--- IdeaSpendWeb/src/app/transactions/transactions-overview/transactions-overview.component.ts
+++ IdeaSpendWeb/src/app/transactions/transactions-overview/transactions-overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../_services/auth.service';
+import {Product} from '../../_model/product';
+import {TransactionService} from '../../_services/transaction.service';
+import {Transaction} from '../../_model/transaction';
 
 @Component({
   selector: 'app-transactions-overview',
@@ -6,15 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions-overview.component.css']
 })
 export class TransactionsOverviewComponent implements OnInit {
+  transactions: Transaction[];
 
-  constructor() { }
+  constructor(private authService: AuthService,
+                      private transactionService: TransactionService) { }
 
   ngOnInit(): void {
+    this.loadTransactions();
   }
 
-  //function to return list of numbers from 0 to n-1 
-  numSequence(n: number): Array<number> { 
-    return Array(n); 
-  } 
-  
+  loadTransactions() {
+    if (this.authService.loggedIn()){
+      this.transactionService.getTransactions(this.authService.decodedToken.nameid)
+        .subscribe((transactions: Transaction[]) => {this.transactions = transactions;})
+    }
+  }
+
+  //function to return list of numbers from 0 to n-1
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
+
 }
